980bad05301108e2b62e2ba71227077229c9ff6f
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-27 08:15:42 +0300

    strings in a tuple concatenation

--- tupleConcatenation.py
+++ tupleConcatenation.py
@@ -1,1 +1,10 @@
 # given a tuple of strings concatenate all the strings and print the result
+
+num = int(input("Enter the size of your tuple:"))
+words = tuple()
+
+for i in range(num):
+    word = input("Enter a word:")
+    words += (word,)
+    
+    print("".join(words))
